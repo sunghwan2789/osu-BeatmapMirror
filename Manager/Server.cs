ecd344f9f95d99923ea7d2f5b24212790f48a4b7
Sunghwan Bang <sunghwan2789@gmail.com>
2020-09-29 23:04:49 +0900

    refactor: use using statements

--- Manager/Server.cs
+++ Manager/Server.cs
@@ -25,12 +25,14 @@ namespace Manager
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            // Release current synchronization context for next services.
             await Task.Yield();
 
-            var listener = new HttpListener
+            using var listener = new HttpListener
             {
                 IgnoreWriteExceptions = true,
             };
+
             foreach (var prefix in Settings.Prefix.Split(','))
             {
                 listener.Prefixes.Add($"http://{prefix}");
@@ -39,36 +41,14 @@ namespace Manager
 
             Logger.LogInformation("Start listening.");
             listener.Start();
+
             try
             {
-                using (stoppingToken.Register(() => listener.Stop()))
-                {
-                    while (listener.IsListening && !stoppingToken.IsCancellationRequested)
-                    {
-                        var context = await listener.GetContextAsync();
-                        if (!context.Request.IsWebSocketRequest ||
-                            (Settings.TLSOnly && !context.Request.IsSecureConnection))
-                        {
-                            if (string.IsNullOrEmpty(Settings.Fallback))
-                            {
-                                context.Response.StatusCode = 400;
-                            }
-                            else
-                            {
-                                context.Response.Redirect(Settings.Fallback);
-                            }
-                            context.Response.Close();
-                            continue;
-                        }
+                using var _ = stoppingToken.Register(() => listener.Stop());
 
-                        try
-                        {
-                            var wsContext = await context.AcceptWebSocketAsync(null);
-                            //Log.Write(wsContext.WebSocket.GetHashCode() + " AcceptWebSocketAsync");
-                            new Client(wsContext.WebSocket).Listen();
-                        }
-                        catch { }
-                    }
+                while (listener.IsListening && !stoppingToken.IsCancellationRequested)
+                {
+                    await ProcessRequestAsync(await listener.GetContextAsync());
                 }
             }
             catch (OperationCanceledException) { }
@@ -82,6 +62,32 @@ namespace Manager
                 listener.Close();
                 ApplicationLifetime.StopApplication();
             }
+        }
+
+        private async Task ProcessRequestAsync(HttpListenerContext context)
+        {
+            if (!context.Request.IsWebSocketRequest
+                || (Settings.TLSOnly && !context.Request.IsSecureConnection))
+            {
+                if (string.IsNullOrEmpty(Settings.Fallback))
+                {
+                    context.Response.StatusCode = 400;
+                }
+                else
+                {
+                    context.Response.Redirect(Settings.Fallback);
+                }
+                context.Response.Close();
+                return;
+            }
+
+            try
+            {
+                var wsContext = await context.AcceptWebSocketAsync(null);
+                //Log.Write(wsContext.WebSocket.GetHashCode() + " AcceptWebSocketAsync");
+                new Client(wsContext.WebSocket).Listen();
+            }
+            catch { }
         }
     }
 }
